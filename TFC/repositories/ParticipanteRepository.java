8aff0740269d327557fdcaa48a2ef609ee090f67
AdrielRC05 <adriel.ruano@iessanmamede.com>
2025-05-07 17:53:59 +0200

    Redundancia eliminada

--- TFC/repositories/ParticipanteRepository.java
+++
@@ -1,8 +0,0 @@
-package ad.TFC.repositories;
-
-import ad.TFC.models.Participante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParticipanteRepository extends JpaRepository<Participante, Long> {}
